f9efdeb56971597f6fe3cfcf6b5924a04e3d6ee1
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 19:44:57 +0530

    Add demo to find maximum of two numbers using fmax in C++

---
+++ C++_Basics/fmax_demo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	float a,b;
+	cout<<"enter the value of a- ";
+	cin>>a;
+	cout<<"enter the value of b- ";
+	cin>>b;
+	cout<<fmax(a,b);
+}
