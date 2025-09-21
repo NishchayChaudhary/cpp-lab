cf30f69052602dc01fcfd92cf86849f2ee2a07d0
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 18:20:53 +0530

    Add demo for array input and output in C++

---
+++ C++_Basics/array_demo.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[4], i;
+
+    // Input 4 elements
+    for(i = 0; i < 4; i++)
+    {
+        cin >> a[i];
+    }
+
+    cout << "\n";
+
+    // Print the elements
+    for(i = 0; i < 4; i++)
+    {
+        cout << a[i] << "\n";
+    }
+
+    return 0;
+}
+
