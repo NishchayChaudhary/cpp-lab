93a3895f19a083c0cca790043d807209f0f15f25
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 20:37:51 +0530

    Add demo to swap specific characters in a string

---
+++ C++_Basics/char_swap_demo.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int j,k,l,n;
+    string a = "and zone point";
+    string b = "a";
+    string c = "p";
+    string d = "z";
+    int len=a.length();
+    for(int i=0;i<=len;i++)
+    {
+    	if(a[i]==b[0])
+    	{
+    		a[i]==a[n];
+    		a[n]==a[i];
+    		j=="b";
+		}
+		else 
+		{
+			continue;
+		}
+	}
+	for(int i=0;i<=len;i++)
+	{
+		if(a[i]==c[0])
+		{
+			k=a[i];
+			k="n";
+			
+		}
+		else
+		{
+			continue;
+		}
+	}
+	for(int i=0;i<=len;i++)
+	{
+		if(a[i]==c[0])
+		{
+			l=a[i];
+			l="n";
+		}
+		else
+		{
+			continue;
+		}
+    }
+    cout<<a[i];
+		
+
+}
