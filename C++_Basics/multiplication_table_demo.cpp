6a48f5b15627722483398b4a6ac6017a9bbdd502
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 20:07:16 +0530

    Add demo to print multiplication table of a number in C++

---
+++ C++_Basics/multiplication_table_demo.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,no,j;
+	cout<<"enter your number";
+	cin>>no;
+	for(i=1;i<=10;i++)
+	{
+		j=no*i;
+		cout<<j;
+		cout<<"\n";
+	}
+			
+}
