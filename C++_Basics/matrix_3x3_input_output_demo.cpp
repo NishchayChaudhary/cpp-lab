84352e017423a044d133324a2c790af2e2c39a51
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 18:25:34 +0530

    Add 3x3 matrix input/output demo in C++

---
+++ C++_Basics/matrix_3x3_input_output_demo.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[3][3],i,j;
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<3;j++)
+		{
+			cin>>a[i][j]; 
+		}
+	}
+	
+	cout<<"\n\n\n";
+	for(int i=0;i<3;i++)
+	{	
+		for(int j=0;j<3;j++)
+		{
+			cout<<a[i][j]<<"\t";
+			
+		}
+		cout<<"\n\n";
+	}
+	return 0;
+}
