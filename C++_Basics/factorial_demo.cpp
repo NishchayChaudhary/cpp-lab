f7bad912f3a0088c3fa61d5610f007243507dc9f
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 19:18:52 +0530

    Add demo to calculate factorial of a number in C++

---
+++ C++_Basics/factorial_demo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Function to calculate factorial
+int factorial(int n) {
+    if(n <= 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int n = 5;
+    cout << "Factorial of " << n << " is: " << factorial(n) << endl;
+    return 0;
+}
