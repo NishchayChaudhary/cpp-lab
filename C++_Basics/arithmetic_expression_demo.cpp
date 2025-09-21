a9da79a531e0b2a88515f8beb20d48c6f62fd39f
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 20:54:04 +0530

    Add demo for arithmetic expression (a*b-b*c)/(d-e)

---
+++ C++_Basics/arithmetic_expression_demo.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,c,d,e,f;
+	cout<<"(a*b-b*c)/(d-e)"<<"\n";
+	cout<<"enter value of a = ";
+	cin>>a;
+	cout<<"enter value of b = ";
+	cin>>b;
+	cout<<"enter value of c = ";
+	cin>>c;
+	cout<<"enter value of d = ";
+	cin>>d;
+	cout<<"enter value of e = ";
+	cin>>e;
+	f=(a*b-b*c)/(d-e);
+	cout<<f;
+	return 0;
+}
