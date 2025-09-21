35e64701996c5510f5c66dce6128ff9bafb2f647
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 19:02:31 +0530

    Add demo to show do-while loop in C++

---
+++ C++_Basics/do_while_demo.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter your number: ";
+    cin >> n;
+
+    do
+    {
+        cout << "special learns institute\n";
+        n++;
+    }
+    while(n <= 8);
+
+    return 0;
+}
