5aa16acc8e1a700e74ec291390e317ee75f425fe
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 19:16:21 +0530

    Add demo to find absolute value of a number in C++

--- C++_Basics/absolute_value_demo.cpp
+++ C++_Basics/absolute_value_demo.cpp
@@ -1,10 +1,28 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
+
 int main()
 {
-	float a;
-	cout<<"enter your number- ";
-	cin>>a;
-	cout<<abs(a);
+    int a;
+    cout << "Enter your number: ";
+    cin >> a;
+
+    cout << "Absolute value: " << abs(a) << endl;
+
+    return 0;
+}
+
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Enter your number: ";
+    cin >> a;
+
+    cout << "Absolute value: " << abs(a) << endl;
+
+    return 0;
 }
