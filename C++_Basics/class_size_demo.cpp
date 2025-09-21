5f149263ae16894d98f1dec96ce211be52ab582a
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 18:52:20 +0530

    Add demo to show size of a class object in C++

---
+++ C++_Basics/class_size_demo.cpp
@@ -0,0 +1,21 @@
+ #include<iostream>
+ using namespace std;
+ class first
+ {
+ 	public:
+
+ 	// 1gb = 1024 ; 
+	 int health;
+ 	char level;
+ 	char khgbj;
+ 	char jjdnskj;
+ 
+ 	
+ };
+ int main()
+ {
+ 	first first1;
+ 	cout<<"size:"<<sizeof(first1)<<endl;
+ 	return 0;
+ }
+ 
