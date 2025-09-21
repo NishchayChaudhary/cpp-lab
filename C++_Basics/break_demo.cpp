c7796e2d831897b5a1c6d6d118b7b61ed0126a2a
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 18:38:47 +0530

    Add demo for break statement in for loop in C++

---
+++ C++_Basics/break_demo.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i, n = 10;
+    for(i = 1; i <= n; i++)
+    {
+        if(i == 5)
+        {
+            break;
+        }
+        else
+        {
+            cout << "special learns institute\n";
+        }
+    }
+    return 0;
+}
