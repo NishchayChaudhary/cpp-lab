e53340eb941cc2a1ee6ea069f946221ce3a80397
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 18:50:23 +0530

    Add demo to find ceiling of a number in C++

---
+++ C++_Basics/ceil_demo.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    float a;
+    cout << "Enter your number: ";
+    cin >> a;
+
+    cout << "Ceiling value: " << ceil(a) << endl;
+
+    return 0;
+}
