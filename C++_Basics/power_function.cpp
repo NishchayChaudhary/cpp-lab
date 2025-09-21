2191f7305f762868934b2751856cf085e5cf9f96
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 22:53:37 +0530

    Add demo program for power function (x^4)

---
+++ C++_Basics/power_function.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int x, y = 4;
+    cout << "Enter your number: ";
+    cin >> x;
+    cout << pow(x, y);
+    return 0;
+}
