7b6f35a50510e4b34d646a94b585e9f53711beb8
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 20:03:20 +0530

    Add demo to print odd numbers from 1 to 50 in C++

---
+++ C++_Basics/odd_numbers_demo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i = 1; i <= 50; i++)
+    {
+        if(i % 2 == 1)
+        {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
