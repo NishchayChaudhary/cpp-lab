9531260b3714f2001350983542f4cc1f104745c4
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 20:13:09 +0530

    Add library struct demo to store and display book details in C++

--- C++_Basics/library_struct_demo.cpp
+++ C++_Basics/library_struct_demo.cpp
@@ -17,24 +17,26 @@ int main() {
 
     Lib book[n];
 
-    for (int i = 0; i < n; i++) 
-	{
+    for (int i = 0; i < n; i++) {
+        cout << "Enter the name of the Book: ";
+        getline(cin, book[i].name);
 
+        cout << "Enter the Price of the book: ";
+        cin >> book[i].price;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        
+        cout << "Enter the name of the Author of the Book: ";
+        getline(cin, book[i].author);
 
- 		cout << "Enter book name: ";
- 		getline (cin,book[i].name);
+        cout << "\n";
 
-		cout << "Enter price: ";
-  		cin>>(book[i].price);
-
-  		cout << "Enter book author name(press # to complete): ";
-  		getline(cin,book[i].author, '#');	//# is a delimiter 
-
- 		cout << "\nEntered details are:\n";
-  		cout << "book name: " << book[i].name << endl;
- 		cout << "price of  book: " << book[i].price << endl;
-  		cout << "Author name of book: " << book[i].author << endl;
+        cout << "The Details of the Book You entered are:" << endl;
+        cout << "The Book name is " << book[i].name << endl;
+        cout << "The Author of the Book is " << book[i].author << endl;
+        cout << "The Price of the Book is " << book[i].price << endl;
+        cout << "\n";
     }
 
     return 0;
 }
+
