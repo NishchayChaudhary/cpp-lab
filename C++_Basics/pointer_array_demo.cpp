143f6c665126867a662be9898863fd4242baaa78
Nishchay Chaudhary <chaudharynishchay276@gmail.com>
2025-09-21 23:07:24 +0530

    Add pointer demo program with static array

--- C++_Basics/pointer_array_demo.cpp
+++ C++_Basics/pointer_array_demo.cpp
@@ -1,12 +1,13 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main() {
-    int arr[5] = {42, 31, 41, 28, 76};
-    int *ptr = &arr[0];  // pointer to first element
-    ptr++;                // move to next element
-
-    cout << *ptr;         // prints 31
-
-    return 0;
+int main()
+{
+	int arr[5]={42,31,41,28,76};
+	int *ptr;
+	ptr=&arr[0];
+	ptr++;
+	cout<<*ptr;
+	return 0;
 }
+
+
